3df17de3833c6261a92ae7fe37cf84df5be4b5c4
PaolaGuedes <paolaguedesbacca2002@gmail.com>
2021-09-12 15:32:21 -0300

    pagina inicial e cadastro

---
+++ components/bg.php
@@ -0,0 +1,24 @@
+<style>
+    .div-bg {
+        height: 300px;
+        width: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        z-index: -1;
+        filter:brightness(60%);
+        box-shadow: 1px 1px 5px rgb(167, 103, 1);
+        overflow: hidden;
+    }
+
+    .bg {
+        width: 100%;
+        object-fit: cover;
+    }
+</style>
+
+<div class="div-bg">
+    <img class="bg" src="../img/bg-books.jpg">
+</div>
