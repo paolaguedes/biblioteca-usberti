c3946232105dc3537a7f57435bd6fe1ed25af9c7
paolaguedes <53832972+paolaguedes@users.noreply.github.com>
2021-10-16 15:27:53 -0300

    heroku

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
     $host = "us-cdbr-east-04.cleardb.com";
-    $user = "b092560b1a05c8";
-    $password = "b2ed54e0";
-    $database = "heroku_165c5fc426a3b16";
+    $user = "bbda9df28166a0";
+    $password = "2234e071";
+    $database = "heroku_0cbb8d544ec9fd5";
 
     $conexao = new mysqli($host, $user, $password, $database);
 ?>
