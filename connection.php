bec2f9fe734b457dca9091f5793260d99ce94e7c
PaolaGuedes <paolaguedesbacca2002@gmail.com>
2021-11-05 10:13:51 -0300

    update connection

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 
-    $host = "localhost";
-    $user = "root";
-    $password = "";
+    $host = "us-cdbr-east-04.cleardb.com";
+    $user = "b5d2370ac5b993";
+    $password = "c924e9eb";
     $database = "biblioteca";
 
     $conexao = mysqli_connect($host, $user, $password, $database);
