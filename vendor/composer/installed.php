28fcda73b4b7ba4c01be5ec97cc8d81747862d0e
PaolaGuedes <paolaguedesbacca2002@gmail.com>
2021-11-05 09:11:40 -0300

    project

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f6b583d6df74dbde52b4158a1c9bc627ced5f6a8',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f6b583d6df74dbde52b4158a1c9bc627ced5f6a8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
